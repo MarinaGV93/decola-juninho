69d3ee67fc7adea2caa7dad612cbf2870703b7d1
Felipe Santiago Morais <fillikz@gmail.com>
2025-02-10 12:23:04 -0300

    feat(faqs): enhance FAQ interaction with icon rotation and improved transitions

--- assets/js/faqs.js
+++ assets/js/faqs.js
@@ -1,18 +1,26 @@
 const faqsAnswer = document.querySelectorAll(".answer");
 const faqsQuestion = document.querySelectorAll(".open-answer-button");
+const plusIcon = document.querySelectorAll(".plus-icon");
 
 faqsQuestion.forEach((question, index) => {
   question.addEventListener("click", () => {
     const currentAnswer = faqsAnswer[index];
+    const currentIcon = plusIcon[index];
     const activeAnswer = document.querySelector(".active");
+    const activeIcon = document.querySelector(".icon-active");
 
     if (activeAnswer) {
       if (activeAnswer === currentAnswer)
-        return currentAnswer.classList.remove("active");
+        return (
+          currentAnswer.classList.remove("active"),
+          currentIcon.classList.remove("icon-active")
+        );
 
       activeAnswer.classList.remove("active");
+      activeIcon.classList.remove("icon-active");
     }
 
     currentAnswer.classList.add("active");
+    currentIcon.classList.add("icon-active");
   });
 });
