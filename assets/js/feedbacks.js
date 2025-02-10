51b02f817550a8597e1b3012bbef012fa3539b54
Felipe Santiago Morais <fillikz@gmail.com>
2025-02-10 15:30:40 -0300

    feat(feedbacks-section): configure swiper slides into section

--- assets/js/feedbacks.js
+++ assets/js/feedbacks.js
@@ -1,18 +1,28 @@
-const feedbacks = document.querySelectorAll(".feedback");
-
-const updateOpacityFeedbacks = () => {
-  feedbacks.forEach((item) => item.classList.remove("opacity"));
-
-  if (window.innerWidth < 768) {
-    feedbacks[0].classList.add("opacity");
-    feedbacks[1].classList.add("opacity");
-    feedbacks[feedbacks.length - 1].classList.add("opacity");
-    feedbacks[feedbacks.length - 2].classList.add("opacity");
-  } else {
-    feedbacks[0].classList.add("opacity");
-    feedbacks[feedbacks.length - 1].classList.add("opacity");
-  }
-};
-
-updateOpacityFeedbacks();
-window.addEventListener("resize", updateOpacityFeedbacks);
+document.addEventListener("DOMContentLoaded", () => {
+  // eslint-disable-next-line
+  new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: true,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      480: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+      1200: {
+        slidesPerView: 5,
+      },
+    },
+  });
+});
