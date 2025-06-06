596c10927672c8c15384af9ec5a5f31bb233da4d
Marina <marinasgv93@gmail.com>
2025-06-06 16:11:38 -0300

    feat: add swiper navigation buttons

--- assets/js/feedbacks.js
+++ assets/js/feedbacks.js
@@ -1,28 +1,58 @@
 document.addEventListener("DOMContentLoaded", () => {
   // eslint-disable-next-line
-  new Swiper(".swiper", {
-    direction: "horizontal",
+  // new Swiper(".swiper", {
+  //   direction: "horizontal",
+  //   loop: true,
+  //   spaceBetween: 20,
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //   },
+  //   breakpoints: {
+  //     0: {
+  //       slidesPerView: 1,
+  //     },
+  //     480: {
+  //       slidesPerView: 2,
+  //     },
+  //     768: {
+  //       slidesPerView: 3,
+  //     },
+  //     1024: {
+  //       slidesPerView: 4,
+  //     },
+  //     1200: {
+  //       slidesPerView: 5,
+  //     },
+  //   },
+  // });
+  const swiper = new Swiper('.feedbacks-container.swiper', {
     loop: true,
-    spaceBetween: 20,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
     pagination: {
-      el: ".swiper-pagination",
+      el: '.swiper-pagination',
+      clickable: true,
     },
+    slidesPerView: 1,
+    spaceBetween: 24,
     breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      480: {
-        slidesPerView: 2,
-      },
-      768: {
-        slidesPerView: 3,
-      },
-      1024: {
-        slidesPerView: 4,
-      },
-      1200: {
-        slidesPerView: 5,
-      },
-    },
+          0: {
+            slidesPerView: 1,
+          },
+          480: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+          1200: {
+            slidesPerView: 5,
+          },
+        },
   });
 });
